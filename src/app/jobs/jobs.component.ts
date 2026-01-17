20e8c4e087a2d113f23929a8d256282774b8eb30
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-01-17 15:24:34 +0100

    Angular v19 migration successfull

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-jobs',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  templateUrl: './jobs.component.html',
-  styleUrl: './jobs.component.css'
+    selector: 'app-jobs',
+    imports: [CommonModule, ReactiveFormsModule, FormsModule],
+    templateUrl: './jobs.component.html',
+    styleUrl: './jobs.component.css'
 })
 export class JobsComponent {
   
