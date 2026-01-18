53190471166f02f4a0262bb9e48f8e517bf1b497
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-01-18 16:30:50 +0100

    Angular Material installed

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,18 +1,19 @@
-
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { APIConnectionService } from '../../../Services/apiconnection.service';
 import { HttpEventType } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
     selector: 'app-jobs',
-    imports: [ReactiveFormsModule, FormsModule],
+    imports: [ReactiveFormsModule, FormsModule, MatProgressBarModule],
     templateUrl: './jobs.component.html',
     styleUrl: './jobs.component.css'
 })
 export class JobsComponent {
 
   jobLength: number = 59;
+  progressBarValue: number = 20;
   constructor(private apiConnectionService: APIConnectionService) { }
 
   
@@ -21,6 +22,7 @@ export class JobsComponent {
     next: (event: any) => {
       if (event.type === HttpEventType.DownloadProgress) {
         console.log('Current stream buffer:', event.partialText);
+        console.log('Substring:', event.partialText.substring(5, 6));
       } else if (event.type === HttpEventType.Response) {
         console.log('Full stream finished:', event.body);
       }
