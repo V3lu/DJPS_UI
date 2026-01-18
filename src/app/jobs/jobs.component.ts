6f813b7419ccb629836287ae9d8880c2a32fb419
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-01-18 17:08:02 +0100

    Multiple streams are read are the same time

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -12,21 +12,41 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 })
 export class JobsComponent {
 
-  jobLength: number = 59;
-  progressBarValue: number = 0;
+  jobLength1: number = 59;
+  jobLength2: number = 40;
+  progressBarValue1: number = 0;
+  progressBarValue2: number = 0;
   constructor(private apiConnectionService: APIConnectionService) { }
 
   
-  StartJob() {
-    const input = document.getElementById('inputJobLength') as HTMLInputElement;
-    this.jobLength = parseInt(input.value);
+  StartJob1() {
+    const input = document.getElementById('inputJobLength1') as HTMLInputElement;
+    this.jobLength1 = parseInt(input.value);
 
-    this.apiConnectionService.StartJob(this.jobLength).subscribe({
+    this.apiConnectionService.StartJob(this.jobLength1).subscribe({
       next: (event: any) => {
         if (event.type === HttpEventType.DownloadProgress) {
           let lastChunk = event.partialText.substring(event.partialText.length - 19);
           let strings = lastChunk.split(' ');
-          this.progressBarValue = parseInt(strings[1]);
+          this.progressBarValue1 = parseInt(strings[1]);
+        } else if (event.type === HttpEventType.Response) {
+          console.log('Full stream finished:', event.body);
+        }
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  StartJob2() {
+    const input = document.getElementById('inputJobLength2') as HTMLInputElement;
+    this.jobLength2 = parseInt(input.value);
+
+    this.apiConnectionService.StartJob(this.jobLength2).subscribe({
+      next: (event: any) => {
+        if (event.type === HttpEventType.DownloadProgress) {
+          let lastChunk = event.partialText.substring(event.partialText.length - 19);
+          let strings = lastChunk.split(' ');
+          this.progressBarValue2 = parseInt(strings[1]);
         } else if (event.type === HttpEventType.Response) {
           console.log('Full stream finished:', event.body);
         }
